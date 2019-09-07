6672a7b6b55a8fec2056388daf619182b8a6d908
henry-alakazhang <henry.alakazhang@gmail.com>
2019-09-07 21:46:35 +1000

    Calculate evolution stats using base Pokemon

--- src/core/pokemon.model.ts
+++ src/core/pokemon.model.ts
@@ -50,6 +50,7 @@ export interface Attack {
 }
 
 export interface Pokemon {
+  /** Name for display in-game */
   readonly displayName: string;
   readonly categories: ReadonlyArray<Category>;
   /** Pokemon tier / shop cost */
@@ -64,60 +65,22 @@ export interface Pokemon {
   readonly basicAttack: Attack;
   readonly move?: Move;
   readonly evolution?: PokemonName;
+  /**
+   * Stage 1 Pokemon that this evolves from
+   * Used for figuring out shop pools / selling evolved Pokemon
+   */
+  readonly base: PokemonName;
   /** Evolution stage */
   readonly stage: 1 | 2 | 3;
 }
 
 /**
- * The base data for all Pokemon
+ * The raw data for all the Pokemon bases (fully evolved)
+ * Pokemon at different evolution stages is extrapolated from this
  */
-const rawPokemonData = {
+const basePokemonData = {
   litwick: {
-    displayName: 'Litwick',
-    categories: ['fire', 'ghost'],
-    tier: 2,
-    maxHP: 50,
-    maxPP: 15,
-    attack: 30,
-    defense: 55,
-    specAttack: 65,
-    specDefense: 55,
-    speed: 20,
-    basicAttack: {
-      range: 3,
-      stat: 'specAttack',
-      projectile: {
-        key: 'firedart',
-        speed: 200,
-      },
-    },
-    evolution: 'lampent',
-    stage: 1,
-  },
-  lampent: {
-    displayName: 'Lampent',
-    categories: ['fire', 'ghost'],
-    tier: 2,
-    maxHP: 60,
-    maxPP: 15,
-    attack: 40,
-    defense: 60,
-    specAttack: 95,
-    specDefense: 60,
-    speed: 55,
-    basicAttack: {
-      range: 3,
-      stat: 'specAttack',
-      projectile: {
-        key: 'firedart',
-        speed: 200,
-      },
-    },
-    evolution: 'chandelure',
-    stage: 2,
-  },
-  chandelure: {
-    displayName: 'Chandelure',
+    base: 'litwick',
     categories: ['fire', 'ghost'],
     tier: 2,
     maxHP: 60,
@@ -135,49 +98,10 @@ const rawPokemonData = {
         speed: 200,
       },
     },
-    stage: 3,
   },
   fletchling: {
-    displayName: 'Fletchling',
+    base: 'fletchling',
     categories: ['fire', 'flying'],
-    tier: 1,
-    maxHP: 45,
-    maxPP: 10,
-    attack: 50,
-    defense: 43,
-    specAttack: 40,
-    specDefense: 38,
-    speed: 62,
-    basicAttack: {
-      range: 1,
-      stat: 'attack',
-    },
-    move: braveBird,
-    evolution: 'fletchinder',
-    stage: 1,
-  },
-  fletchinder: {
-    displayName: 'Fletchinder',
-    categories: ['fire', 'flying'],
-    tier: 1,
-    maxHP: 62,
-    maxPP: 10,
-    attack: 73,
-    defense: 55,
-    specAttack: 56,
-    specDefense: 52,
-    speed: 84,
-    basicAttack: {
-      range: 1,
-      stat: 'attack',
-    },
-    move: braveBird,
-    evolution: 'talonflame',
-    stage: 2,
-  },
-  talonflame: {
-    displayName: 'Talonflame',
-    categories: ['fire', 'flying', 'physical attacker'],
     tier: 1,
     maxHP: 78,
     maxPP: 10,
@@ -191,10 +115,9 @@ const rawPokemonData = {
       stat: 'attack',
     },
     move: braveBird,
-    stage: 3,
   },
   rotomw: {
-    displayName: 'Rotom-W',
+    base: 'rotomw',
     categories: ['water', 'electric'],
     tier: 4,
     maxHP: 50,
@@ -208,43 +131,79 @@ const rawPokemonData = {
       range: 1,
       stat: 'specAttack',
     },
-    stage: 1,
+  },
+} as const;
+
+/**
+ * Returns the evolution of a Pokemon, with its stats scaled based on the evolution stage.
+ */
+function getEvolution(pokemon: keyof typeof basePokemonData, stage: 1 | 2 | 3) {
+  const basePokemon = basePokemonData[pokemon];
+  // maxHP and damage scale with Pokemon level
+  // stage 1 =x2=> stage 2 =x2=> stage 3
+  let multi = 1;
+  if (stage === 2) {
+    multi = 2;
+  }
+  if (stage === 3) {
+    multi = 4;
+  }
+
+  return {
+    ...basePokemon,
+    maxHP: basePokemon.maxHP * multi,
+    attack: basePokemon.attack * multi,
+    specAttack: basePokemon.specAttack * multi,
+    stage,
+  };
+}
+
+/**
+ * The raw Pokemon data for all Pokemon, including evolutions.
+ * Uses the BasePokemon as a basis
+ */
+const rawPokemonData = {
+  litwick: {
+    ...getEvolution('litwick', 1),
+    displayName: 'Litwick',
+    evolution: 'lampent',
+  },
+  lampent: {
+    ...getEvolution('litwick', 2),
+    displayName: 'Lampent',
+    evolution: 'chandelure',
+  },
+  chandelure: {
+    ...getEvolution('litwick', 3),
+    displayName: 'Chandelure',
+  },
+  fletchling: {
+    ...getEvolution('fletchling', 1),
+    displayName: 'Fletchling',
+    evolution: 'fletchinder',
+  },
+  fletchinder: {
+    ...getEvolution('fletchling', 2),
+    displayName: 'Fletchinder',
+    evolution: 'talonflame',
+  },
+  talonflame: {
+    ...getEvolution('fletchling', 3),
+    displayName: 'Talonflame',
+  },
+  rotomw: {
+    ...getEvolution('rotomw', 1),
+    displayName: 'Rotom Wash',
     evolution: 'rotomw-2',
   },
   'rotomw-2': {
-    displayName: 'Rotom-W',
-    categories: ['water', 'electric'],
-    tier: 4,
-    maxHP: 50,
-    maxPP: 12,
-    attack: 65,
-    defense: 107,
-    specAttack: 105,
-    specDefense: 107,
-    speed: 86,
-    basicAttack: {
-      range: 1,
-      stat: 'specAttack',
-    },
+    ...getEvolution('rotomw', 2),
+    displayName: 'Rotom Wash',
     evolution: 'rotomw-3',
-    stage: 2,
   },
   'rotomw-3': {
-    displayName: 'Rotom-W',
-    categories: ['water', 'electric'],
-    tier: 4,
-    maxHP: 50,
-    maxPP: 12,
-    attack: 65,
-    defense: 107,
-    specAttack: 105,
-    specDefense: 107,
-    speed: 86,
-    basicAttack: {
-      range: 1,
-      stat: 'specAttack',
-    },
-    stage: 3,
+    ...getEvolution('rotomw', 3),
+    displayName: 'Rotom Wash',
   },
 } as const;
 
