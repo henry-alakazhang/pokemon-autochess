f7c083f98fd7b50816764d6e921e41687efe4061
Henry <henry.alakazhang@gmail.com>
2019-12-24 11:31:57 +1100

    Make Razor Wind actually hit in an aoe

--- src/core/moves/razor-wind.ts
+++ src/core/moves/razor-wind.ts
@@ -1,4 +1,5 @@
 import { Move, MoveConfig } from '../move.model';
+import { flatten } from '../../helpers';
 
 /**
  * Razor Wind - Shiftry line's move
@@ -6,7 +7,7 @@ import { Move, MoveConfig } from '../move.model';
  * Starts a whirlwind which triggers after 2 seconds,
  * dealing significant damage over time to an area around the target
  *
- * TODO: actually make it AOE
+ * TODO: add custom targetting to hit maximum AoE
  */
 export const razorWind: Move = {
   displayName: 'Razor Wind',
@@ -18,8 +19,8 @@ export const razorWind: Move = {
       '/'
     )} damage over 2 seconds`;
   },
-  range: 1,
-  use({ scene, user, target, onComplete }: MoveConfig) {
+  range: 3,
+  use({ scene, user, target, board, onComplete }: MoveConfig) {
     // double-hopping animation
     scene.add.tween({
       targets: [user],
@@ -31,7 +32,7 @@ export const razorWind: Move = {
       onComplete: () => {
         // animation: small spinny whirlwind effect below the poekmon
         const base = scene.add
-          .sprite(target.x, target.y + 20, 'razor-wind-base')
+          .sprite(target.x + 35, target.y + 35, 'razor-wind-base')
           .setDepth(-1)
           .play('razor-wind-base');
         // turn is over after casting
@@ -40,22 +41,48 @@ export const razorWind: Move = {
         window.setTimeout(() => {
           base.destroy();
           const wind = scene.add
-            .sprite(target.x, target.y, 'razor-wind-wind')
+            .sprite(target.x + 35, target.y + 35, 'razor-wind-wind')
+            .setScale(0.5, 0.5)
             .play('razor-wind-wind');
+          scene.add.tween({
+            targets: [wind],
+            duration: 1000,
+            scaleX: 2,
+            scaleY: 2,
+          });
+          // the code below uses setInterval so the first tick will occur after the tween ends
+
           const dph = this.damage[user.basePokemon.stage - 1] / 3;
-          let hits = 1;
-          user.dealDamage(dph);
-          target.takeDamage(dph);
+          // get board coords of the target Pokemon
+          // TODO move this into a helper / pass it into the data here
+          const targetPokemon = flatten(
+            board.map((col, x) => col.map((pokemon, y) => ({ x, y, pokemon })))
+          ).find(({ pokemon }) => pokemon && pokemon.id === target.id);
+          if (!targetPokemon) {
+            return;
+          }
+          let hits = 0;
           const interval = window.setInterval(() => {
-            user.dealDamage(dph);
-            target.takeDamage(dph);
+            // deal damage to each person in range (2 x 2 square)
+            const targets = [
+              board[targetPokemon.x][targetPokemon.y],
+              board[targetPokemon.x][targetPokemon.y + 1],
+              board[targetPokemon.x + 1][targetPokemon.y],
+              board[targetPokemon.x + 1][targetPokemon.y + 1],
+            ];
+            targets.forEach(pokemon => {
+              if (pokemon && pokemon.side !== user.side) {
+                user.dealDamage(dph);
+                pokemon.takeDamage(dph);
+              }
+            });
             hits++;
-            if (hits >= 3) {
+            if (hits >= 2) {
               wind.destroy();
               window.clearInterval(interval);
             }
           }, 1000);
-        }, 2000);
+        }, 1000);
       },
     });
   },
