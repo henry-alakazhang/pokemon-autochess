a1a2023bd787b7179b7e96cc8a1c4f50b6775728
Henry Zhang <henry-alakazhang@users.noreply.github.com>
2020-09-07 16:32:36 +1000

    Add Zubat line (#54)

---
+++ src/core/moves/leech-life.ts
@@ -0,0 +1,62 @@
+import {
+  calculateDamage,
+  getAttackAnimation,
+  getFacing,
+  getTurnDelay,
+} from '../../scenes/game/combat/combat.helpers';
+import { animations } from '../animations';
+import { Move, MoveConfig } from '../move.model';
+
+/**
+ * Leech Life - Zubat line's move
+ *
+ * Deals damage to a single target and heals the user for half the amount
+ */
+export const leechLife: Move = {
+  displayName: 'Leech Life',
+  type: 'active',
+  damage: [150, 250, 400],
+  defenseStat: 'defense',
+  targetting: 'unit',
+  get description() {
+    return `Deals ${this.damage.join(
+      '/'
+    )} damage to a single target and heals the user for half that amount.`;
+  },
+  range: 1,
+  use({ scene, user, target, onComplete }: MoveConfig<'unit'>) {
+    scene.add.tween({
+      targets: [user],
+      duration: getTurnDelay(user.basePokemon) * 0.15,
+      ...getAttackAnimation(user, getFacing(user, target)),
+      yoyo: true,
+      ease: 'Power1',
+      onComplete: () => onComplete(),
+      onYoyo: () => {
+        const damage = calculateDamage(user, target, {
+          damage: this.damage[user.basePokemon.stage - 1],
+          defenseStat: this.defenseStat,
+        });
+        target.takeDamage(damage);
+
+        // play bee animation on the target
+        const bees = scene.add
+          .sprite(target.x, target.y, 'leech-life')
+          .play('leech-life')
+          .once(Phaser.Animations.Events.SPRITE_ANIMATION_COMPLETE, () => {
+            bees.destroy();
+          });
+        // and move it onto the user
+        scene.add.tween({
+          targets: [bees],
+          duration: animations['leech-life'].duration * 0.75,
+          x: user.x,
+          y: user.y,
+          onComplete: () => {
+            user.heal(damage / 2);
+          },
+        });
+      },
+    });
+  },
+} as const;
--- src/core/pokemon.model.ts
+++ src/core/pokemon.model.ts
@@ -23,6 +23,7 @@ import {
   voltTackle,
   zapCannon,
 } from './moves';
+import { leechLife } from './moves/leech-life';
 
 export type Type =
   | 'normal'
@@ -511,6 +512,23 @@ const basePokemonData = {
     },
     move: meteorMash,
   },
+  zubat: {
+    base: 'zubat',
+    categories: ['poison', 'flying'],
+    tier: 1,
+    maxHP: 85,
+    maxPP: 10,
+    attack: 90,
+    defense: 80,
+    specAttack: 70,
+    specDefense: 80,
+    speed: 130,
+    basicAttack: {
+      range: 1,
+      stat: 'attack',
+    },
+    move: leechLife,
+  },
 } as const;
 
 /**
@@ -927,6 +945,23 @@ const rawPokemonData = {
     ...getEvolution('beldum', 3),
     name: 'metagross',
     displayName: 'Metagross',
+  },
+  zubat: {
+    ...getEvolution('zubat', 1),
+    name: 'zubat',
+    displayName: 'Zubat',
+    evolution: 'golbat',
+  },
+  golbat: {
+    ...getEvolution('zubat', 2),
+    name: 'golbat',
+    displayName: 'Golbat',
+    evolution: 'crobat',
+  },
+  crobat: {
+    ...getEvolution('zubat', 3),
+    name: 'crobat',
+    displayName: 'Crobat',
   },
 } as const;
 
