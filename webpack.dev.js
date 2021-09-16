c6f5f1cecf16c7c30ccd970110a94b128ef66d7a
Henry <henry.alakazhang@gmail.com>
2021-09-16 23:02:10 +1000

    Upgrade to webpack 5

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,73 +1,11 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-  entry: {
-    game: './src/game.ts',
-  },
+module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
-  devServer: {
-    contentBase: './build',
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    pathinfo: false,
-    filename: '[name].js',
-    devtoolModuleFilenameTemplate: '../[resource-path]',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-      },
-      {
-        test: [/\.vert$/, /\.frag$/],
-        use: 'raw-loader',
-      },
-    ],
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      },
-    },
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
   stats: true,
-  plugins: [
-    new webpack.DefinePlugin({
-      'typeof SHADER_REQUIRE': JSON.stringify(false),
-      'typeof CANVAS_RENDERER': JSON.stringify(true),
-      'typeof WEBGL_RENDERER': JSON.stringify(true),
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: './assets',
-          to: './assets',
-          force: true,
-        },
-        {
-          from: './app.css',
-          to: './app.css',
-          force: true,
-        },
-      ],
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-    }),
-  ],
-};
+  devServer: {
+    static: './build',
+  },
+});
