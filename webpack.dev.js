39a179eaefdec69a155e9274ce17b31a3c8b97df
Henry <henry.alakazhang@gmail.com>
2021-09-16 22:23:34 +1000

    Upgrade base deps to 4

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,71 +1,73 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    game: "./src/game.ts"
+    game: './src/game.ts',
   },
-  mode: "development",
-  devtool: "eval-source-map",
+  mode: 'development',
+  devtool: 'eval-source-map',
   devServer: {
-    contentBase: "./build"
+    contentBase: './build',
   },
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, 'build'),
     pathinfo: false,
-    filename: "[name].js",
-    devtoolModuleFilenameTemplate: "../[resource-path]"
+    filename: '[name].js',
+    devtoolModuleFilenameTemplate: '../[resource-path]',
   },
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        use: "ts-loader"
+        use: 'ts-loader',
       },
       {
         test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader"
-      }
-    ]
+        use: 'raw-loader',
+      },
+    ],
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         commons: {
           test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all"
-        }
-      }
-    }
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: ['.tsx', '.ts', '.js'],
   },
   stats: true,
   plugins: [
     new webpack.DefinePlugin({
-      "typeof SHADER_REQUIRE": JSON.stringify(false),
-      "typeof CANVAS_RENDERER": JSON.stringify(true),
-      "typeof WEBGL_RENDERER": JSON.stringify(true)
+      'typeof SHADER_REQUIRE': JSON.stringify(false),
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
     }),
-    new CopyWebpackPlugin([
-      {
-        from: "./assets",
-        to: "./assets",
-        force: true
-      },
-      {
-        from: "./app.css",
-        to: "./app.css",
-        force: true
-      }
-    ]),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './assets',
+          to: './assets',
+          force: true,
+        },
+        {
+          from: './app.css',
+          to: './app.css',
+          force: true,
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "index.html"
-    })
-  ]
+      filename: 'index.html',
+      template: 'index.html',
+    }),
+  ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,29 +1,29 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    game: "./src/game.ts"
+    game: './src/game.ts',
   },
-  mode: "production",
+  mode: 'production',
   output: {
-    path: path.resolve(__dirname, "build"),
-    publicPath: "./",
-    filename: "[name].min.js"
+    path: path.resolve(__dirname, 'build'),
+    publicPath: './',
+    filename: '[name].min.js',
   },
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        use: "ts-loader"
+        use: 'ts-loader',
       },
       {
         test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader"
-      }
-    ]
+        use: 'raw-loader',
+      },
+    ],
   },
   optimization: {
     minimize: true,
@@ -31,37 +31,39 @@ module.exports = {
       cacheGroups: {
         commons: {
           test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all"
-        }
-      }
-    }
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: ['.tsx', '.ts', '.js'],
   },
   stats: false,
   plugins: [
     new webpack.DefinePlugin({
-      "typeof SHADER_REQUIRE": JSON.stringify(false),
-      "typeof CANVAS_RENDERER": JSON.stringify(true),
-      "typeof WEBGL_RENDERER": JSON.stringify(true)
+      'typeof SHADER_REQUIRE': JSON.stringify(false),
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
     }),
-    new CopyWebpackPlugin([
-      {
-        from: "./assets",
-        to: "./assets",
-        force: true
-      },
-      {
-        from: "./app.css",
-        to: "./app.css",
-        force: true
-      }
-    ]),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './assets',
+          to: './assets',
+          force: true,
+        },
+        {
+          from: './app.css',
+          to: './app.css',
+          force: true,
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "index.html"
-    })
-  ]
+      filename: 'index.html',
+      template: 'index.html',
+    }),
+  ],
 };
