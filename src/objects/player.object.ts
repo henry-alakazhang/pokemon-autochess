3fbf6851b9ef551b2910c1487e7228cc9dc410f9
Henry <henry.alakazhang@gmail.com>
2021-09-24 11:32:23 +1000

    Implement AI turns

--- src/objects/player.object.ts
+++ src/objects/player.object.ts
@@ -1,6 +1,7 @@
 import { Category, getSynergyTier, synergyData } from '../core/game.model';
-import { PokemonName } from '../core/pokemon.model';
+import { pokemonData, PokemonName } from '../core/pokemon.model';
 import { flatten, isDefined } from '../helpers';
+import { inBounds } from '../scenes/game/combat/combat.helpers';
 import { CombatBoard } from '../scenes/game/combat/combat.scene';
 import {
   GameScene,
@@ -10,13 +11,14 @@ import {
   getSideboardLocationForCoordinates,
   PokemonLocation,
 } from '../scenes/game/game.scene';
+import { ShopPool } from '../scenes/game/shop.helpers';
 import { PokemonObject } from './pokemon.object';
 import { SynergyMarker } from './synergy-marker.object';
 import { defaultStyle } from './text.helpers';
 
 const MAX_MAINBOARD_POKEMON = 6;
 
-export class Player extends Phaser.GameObjects.GameObject {
+export class Player extends Phaser.GameObjects.Group {
   hp = 100;
   gold = 20;
   /** Consecutive win/loss streak */
@@ -39,14 +41,21 @@ export class Player extends Phaser.GameObjects.GameObject {
   synergies: { category: Category; count: number }[] = [];
   synergyMarkers: { [k in Category]?: SynergyMarker } = {};
 
+  currentShop: PokemonName[];
+
   constructor(
     scene: GameScene,
     public playerName: string,
     x: number,
-    y: number
+    y: number,
+    private readonly pool: ShopPool,
+    private visible = false
   ) {
-    super(scene, 'player');
+    super(scene);
+    super.setVisible(visible);
 
+    // not part of group - always visible
+    // TODO: move to game scene?
     this.nameInList = scene.add.text(
       x,
       y,
@@ -135,6 +144,34 @@ export class Player extends Phaser.GameObjects.GameObject {
     }
   }
 
+  buyPokemon(pokemonName: PokemonName): boolean {
+    const price = pokemonData[pokemonName].tier;
+    if (this.gold < price) {
+      return false;
+    }
+
+    if (!this.canAddPokemonToSideboard()) {
+      return false;
+    }
+
+    this.gold -= pokemonData[pokemonName].tier;
+    this.addPokemonToSideboard(pokemonName);
+    return true;
+  }
+
+  sellPokemon(pokemon: PokemonObject) {
+    if (pokemon.basePokemon.stage === 1) {
+      this.gold += pokemon.basePokemon.tier;
+    } else if (pokemon.basePokemon.stage === 2) {
+      this.gold += pokemon.basePokemon.tier + 2;
+    } else {
+      this.gold += pokemon.basePokemon.tier + 4;
+    }
+
+    this.pool.returnToShop(pokemon.basePokemon.name);
+    this.removePokemon(pokemon);
+  }
+
   addPokemonToSideboard(pokemon: PokemonName) {
     if (!this.canAddPokemonToSideboard()) {
       return;
@@ -148,8 +185,8 @@ export class Player extends Phaser.GameObjects.GameObject {
       ...getCoordinatesForSideboardIndex(empty),
       name: pokemon,
       side: 'player',
-    });
-    this.scene.add.existing(newPokemon);
+    }).setVisible(this.visible);
+    this.add(newPokemon, true);
     this.sideboard[empty] = newPokemon;
 
     /* check evolutions */
@@ -191,7 +228,7 @@ export class Player extends Phaser.GameObjects.GameObject {
    * For optimisation purposes, only checks one Pokemon.
    * This should be fine if it's called every time a new Pokemon is added.
    */
-  applyEvolutions(newPokemon: PokemonObject) {
+  applyEvolutions(newPokemon: PokemonObject): void {
     const evolutionName = newPokemon.basePokemon.evolution;
     if (!evolutionName) {
       return;
@@ -226,7 +263,32 @@ export class Player extends Phaser.GameObjects.GameObject {
       this.markedForEvolution[pokemon.id] = true;
     });
 
-    // play a flashing animation for a bit
+    const addEvolution = () => {
+      // delete old Pokemon
+      samePokemon.forEach(pokemon => {
+        this.markedForEvolution[pokemon.id] = false;
+        this.removePokemon(pokemon);
+      });
+      // add new one
+      const evo = new PokemonObject({
+        scene: this.scene,
+        x: 0,
+        y: 0,
+        name: evolutionName,
+        side: 'player',
+      }).setVisible(this.visible);
+      this.add(evo, true);
+      this.setPokemonAtLocation(evoLocation, evo);
+      return evo;
+    };
+
+    // if not visible, just immediately evolve
+    if (!this.visible) {
+      const evo = addEvolution();
+      return this.applyEvolutions(evo);
+    }
+
+    // otherwise play a flashing animation for a bit
     // not sure how to use tweens to get a flashing trigger of the outline
     // so this just manually creates one using timers
     const flashTimer = this.scene.time.addEvent({
@@ -243,21 +305,7 @@ export class Player extends Phaser.GameObjects.GameObject {
       callback: () => {
         // end animation
         flashTimer.remove();
-        // delete old Pokemon
-        samePokemon.forEach(pokemon => {
-          this.markedForEvolution[pokemon.id] = false;
-          this.removePokemon(pokemon);
-        });
-        // add new one
-        const evo = new PokemonObject({
-          scene: this.scene,
-          x: 0,
-          y: 0,
-          name: evolutionName,
-          side: 'player',
-        });
-        this.scene.add.existing(evo);
-        this.setPokemonAtLocation(evoLocation, evo);
+        const evo = addEvolution();
         // play animation to make it super clear
         evo.setScale(1.5, 1.5);
         this.scene.add.tween({
@@ -281,23 +329,17 @@ export class Player extends Phaser.GameObjects.GameObject {
       getSideboardLocationForCoordinates(pokemon);
     if (!location) {
       // just destroy since it's not displayed anyway?
-      return pokemon.destroy();
+      return this.remove(pokemon, true, true);
     }
 
     if (location.location === 'mainboard') {
-      const existing = this.mainboard[location.coords.x][location.coords.y];
       this.mainboard[location.coords.x][location.coords.y] = undefined;
-      if (existing) {
-        existing.destroy();
-      }
       this.updateSynergies();
     } else {
-      const existing = this.sideboard[location.index];
       this.sideboard[location.index] = undefined;
-      if (existing) {
-        existing.destroy();
-      }
     }
+    // remove, remove from scene and destroy
+    this.remove(pokemon, true, true);
   }
 
   updateSynergies() {
@@ -339,6 +381,11 @@ export class Player extends Phaser.GameObjects.GameObject {
         return b.category > a.category ? -1 : 1;
       });
 
+    if (!this.visible) {
+      // if this is hidden, don't bother rendering synergies.
+      return;
+    }
+
     // hide all synergy markers
     Object.values(this.synergyMarkers).forEach(marker => {
       marker.setVisible(false).setActive(false);
@@ -367,5 +414,36 @@ export class Player extends Phaser.GameObjects.GameObject {
 
   canAddPokemonToSideboard() {
     return this.sideboard.includes(undefined);
+  }
+
+  takeEnemyTurn() {
+    this.currentShop = this.pool.reroll(this.currentShop);
+    console.log('player', this.playerName, 'bought', this.currentShop[4]);
+    this.buyPokemon(this.currentShop[4]);
+    const newPokemon = this.getPokemonAtLocation({
+      location: 'sideboard',
+      index: 0,
+    });
+    if (!newPokemon) {
+      return;
+    }
+
+    // ranged units go in back row, melee in front
+    const y = newPokemon.basePokemon.basicAttack.range === 1 ? 3 : 4;
+    let x = 0;
+    // find a spot in the row where the unit will fit
+    while (
+      inBounds(this.mainboard, { x, y }) &&
+      isDefined(this.mainboard[x][y])
+    ) {
+      x++;
+    }
+    // if such a spot exists, put them there
+    if (
+      inBounds(this.mainboard, { x, y }) &&
+      !isDefined(this.mainboard[x][y])
+    ) {
+      this.movePokemon(newPokemon, { location: 'mainboard', coords: { x, y } });
+    }
   }
 }
--- src/scenes/game/shop.helpers.ts
+++ src/scenes/game/shop.helpers.ts
@@ -1,55 +1,79 @@
-import { buyablePokemon, PokemonName } from '../../core/pokemon.model';
+import {
+  buyablePokemon,
+  pokemonData,
+  PokemonName,
+} from '../../core/pokemon.model';
 
-export const SHOP_POOL_SIZES = {
-  1: 3,
-  2: 3,
-  3: 3,
-  4: 3,
-  5: 3,
+const SHOP_POOL_SIZES = {
+  1: 27,
+  2: 24,
+  3: 21,
+  4: 18,
+  5: 15,
 };
 
 /**
- * Rerolls a shop from a given pool of Pokemon
- * Mutates the pool by returning the previously rolled pokemon,
- * and removing the newly rolled Pokemon
+ * A pool of buyable units which forms a shop.
  */
-export function rerollShop(
-  pool: {
+export class ShopPool {
+  readonly pool: {
     [k in PokemonName]?: number;
-  },
-  currentShop: PokemonName[]
-): PokemonName[] {
-  // return the original Pokemon to the shop (if applicable)
-  currentShop.forEach(pokemon => {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    pool[pokemon]! += 1;
-  });
+  };
 
-  const newShop: PokemonName[] = [];
-  // populate it with new pokemon
-  // we use a for loop here rather than a map because we use `delete`
-  // to remove Pokemon from the shop, rather than marking as undefined
-  // iterating over an array with missing elements doesn't hit the empty spots
-  // TODO: scale the RNG based on how many Pokemon are remaining
-  for (let i = 0; i < currentShop.length; i++) {
-    let pick =
-      buyablePokemon[Math.floor(Math.random() * buyablePokemon.length)];
-    // don't pick if the pool is out of this Pokemon
-    // in theory this will go infinite if we buy too many Pokemon :thinking:
-    while (!pool[pick]) {
-      pick = buyablePokemon[Math.floor(Math.random() * buyablePokemon.length)];
-    }
-
-    // DEBUG CODE: always add the latest Pokemon for debugging purposes
-    if (i === 0) {
-      pick = buyablePokemon[buyablePokemon.length - 1];
-    }
-
-    // take the pokemon out of the pool
-    // non-null assertion here is ok because we literally just checked it
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    pool[pick]! -= 1;
-    newShop.push(pick);
+  constructor() {
+    this.pool = {};
+    buyablePokemon.forEach(pokemon => {
+      this.pool[pokemon] = SHOP_POOL_SIZES[pokemonData[pokemon].tier];
+    });
   }
-  return newShop;
+
+  /**
+   * Empties a pool back into the shop and generates a new one.
+   *
+   * Note: If a unit has been bought, it is expecte to have
+   * been DELETEd from the array. Don't just null it out.
+   */
+  reroll(currentShop: PokemonName[] = Array(5)): PokemonName[] {
+    // return the original Pokemon to the shop (if applicable)
+    currentShop.forEach(pokemon => {
+      this.returnToShop(pokemon);
+    });
+
+    const newShop: PokemonName[] = [];
+    // populate it with new pokemon
+    // we use a for loop here rather than a map because we use `delete`
+    // to remove Pokemon from the shop, rather than marking as undefined
+    // iterating over an array with missing elements doesn't hit the empty spots
+    // TODO: scale the RNG based on how many Pokemon are remaining
+    for (let i = 0; i < currentShop.length; i++) {
+      let pick =
+        buyablePokemon[Math.floor(Math.random() * buyablePokemon.length)];
+      // don't pick if the pool is out of this Pokemon
+      // in theory this will go infinite if we buy out the entire shop :
+      while (!this.pool[pick]) {
+        pick =
+          buyablePokemon[Math.floor(Math.random() * buyablePokemon.length)];
+      }
+
+      // DEBUG CODE: always add the latest Pokemon for debugging purposes
+      if (i === 0) {
+        pick = buyablePokemon[buyablePokemon.length - 1];
+      }
+
+      // take the pokemon out of the pool
+      // non-null assertion here is ok because we literally just checked it
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      this.pool[pick]! -= 1;
+      newShop.push(pick);
+    }
+    return newShop;
+  }
+
+  returnToShop(name: PokemonName) {
+    const pokemon = pokemonData[name];
+    // return more copies for evolved Pokemon
+    const totalCopies = pokemon.stage === 1 ? 1 : pokemon.stage === 2 ? 3 : 9;
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    this.pool[pokemon.base]! += totalCopies;
+  }
 }
