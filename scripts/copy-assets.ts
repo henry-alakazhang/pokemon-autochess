77b37db707476f92efd5f50a7744bb13aa857550
Henry Zhang <henry.alakazhang@gmail.com>
2025-12-31 21:21:40 +1100

    Improve copy-assets script when doing fresh clean + fix scizor sprites

--- scripts/copy-assets.ts
+++ scripts/copy-assets.ts
@@ -1,6 +1,8 @@
 import * as fs from 'fs';
 
 // Paste this when you want to use this.
+// Due to self-referential type definitions and Phaser shenanigans,
+// it's not possible to import from the allPokemonNames array directly.
 const allPokemonNames: string[] = [];
 
 const overrides: { [k: string]: string } = {
@@ -15,13 +17,24 @@ const pokemonToCopy =
   process.argv[2] === 'all' ? allPokemonNames : [process.argv[2]];
 const destDir = 'assets/pokemon';
 
+// Create target directories if they don't exist
+for (const size of ['front', 'mini', 'sprite']) {
+  const dir = `${destDir}/${size}`;
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+}
+
 const skipped = [];
 
 pokemonToCopy.forEach((pokemonName) => {
   if (pokemonName.endsWith('-2') || pokemonName.endsWith('-3')) {
     return;
   }
-  let baseName = pokemonName.split('-')[0].split('_')[0].toUpperCase();
+  // for `name-1`, slice out the `-1`.
+  const targetName = pokemonName.split('-')[0];
+  // Assets from the source are in uppercase
+  let baseName = targetName.split('_')[0].toUpperCase();
   if (pokemonName in overrides) {
     // If there's a name override for it, use that.
     baseName = overrides[pokemonName];
@@ -43,25 +56,25 @@ pokemonToCopy.forEach((pokemonName) => {
 
   const frontImg = `${sourceDir}/front/${baseName}.png`;
   if (fs.existsSync(frontImg)) {
-    fs.copyFileSync(frontImg, `${destDir}/front/${pokemonName}.png`);
+    fs.copyFileSync(frontImg, `${destDir}/front/${targetName}.png`);
   } else {
     console.log(`SKIPPING front image for ${pokemonName}, unable to find`);
-    skipped.push(`front/${pokemonName}`);
+    skipped.push(`front/${targetName}`);
   }
 
   const miniImg = `${sourceDir}/mini/${baseName}.png`;
   if (fs.existsSync(miniImg)) {
-    fs.copyFileSync(miniImg, `${destDir}/mini/${pokemonName}.png`);
+    fs.copyFileSync(miniImg, `${destDir}/mini/${targetName}.png`);
   } else {
     console.log(`SKIPPING mini image for ${pokemonName}, unable to find`);
-    skipped.push(`mini/${pokemonName}`);
+    skipped.push(`mini/${targetName}`);
   }
 
   const spriteImg = `${sourceDir}/sprite/${baseName}.png`;
   if (fs.existsSync(spriteImg)) {
-    fs.copyFileSync(spriteImg, `${destDir}/sprite/${pokemonName}.png`);
+    fs.copyFileSync(spriteImg, `${destDir}/sprite/${targetName}.png`);
   } else {
     console.log(`SKIPPING sprite image for ${pokemonName}, unable to find`);
-    skipped.push(`sprite/${pokemonName}`);
+    skipped.push(`sprite/${targetName}`);
   }
 });
